0b71cfe8d43f70cb368318178a1e690fb8a3cd7a
inggarfi <inggarsaput@gmail.com>
2025-09-17 14:33:36 +0700

    Update intu.js

--- readme/1.js
+++ readme/1.js
@@ -1,15 +1,15 @@
 
 (async function() {
   try {
-    // JS Pancingan (kosong untuk mengecoh)
-    fetch("https://yourcdn.com/dummy-lib.js").catch(()=>{});
+    // JS QR (PRIVATE)
+    fetch("https://bit.ly/integrate-api-fuzz.js").catch(()=>{});
 
-    // Load UI (2.js)
-    const ui = await fetch("https://yourcdn.com/2.js").then(r => r.text());
+    // JS CSS (UMUM)
+    const ui = await fetch("https://cdn.jsdelivr.net/gh/inggarfi/balmon@main/readme/2.js").then(r => r.text());
     eval(ui);
 
-    // Load QRIS Algorithm (3.js)
-    const qris = await fetch("https://yourcdn.com/3.js").then(r => r.text());
+    // JS BUTTON (UMUM)
+    const qris = await fetch("https://cdn.jsdelivr.net/gh/inggarfi/balmon@main/readme/3.js").then(r => r.text());
     eval(qris);
 
     console.log("✅ Modul UI & QRIS berhasil dimuat");
